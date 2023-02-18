c57ccbcee1777a4cf292725ff55b40439e9f9f66
KitsuneYokai <56147653+KitsuneYokai@users.noreply.github.com>
2023-02-18 21:00:30 +0100

    to laptop
    commit to my laptop so i can work while im not at home

--- lib/src/sites/settings/settings_view.dart
+++ lib/src/sites/settings/settings_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../ui.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 import 'settings_controller.dart';
 
@@ -51,7 +52,46 @@ class SettingsView extends StatelessWidget {
               const Text("Debug Stuff:"),
               //create a button to test the tray icon
               ElevatedButton(
-                  onPressed: () {}, child: const Text("Connect to KitsuDeck")),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => WebsocketView()));
+                  },
+                  child: const Text("Open Websocket viewer")),
+            ],
+          )),
+        ));
+  }
+}
+
+// websocket viewer
+class WebsocketView extends StatelessWidget {
+  WebsocketView({
+    super.key,
+  });
+
+  final channel = WebSocketChannel.connect(
+    Uri.parse('ws://192.168.178.23/ws'),
+  );
+  static const routeName = '/websocket';
+  @override
+  Widget build(BuildContext context) {
+    return MainView(
+        title: "KitsuDeck websocket Viewer",
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Expanded(
+              child: Column(
+            children: [
+              const Text("Websocket Viewer"),
+              //create a button to test the tray icon
+              StreamBuilder(
+                stream: channel.stream,
+                builder: (context, snapshot) {
+                  return Text(snapshot.hasData ? '${snapshot.data}' : '');
+                },
+              )
             ],
           )),
         ));
